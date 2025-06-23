2b18778fb2b2817830989b036e2a45d7e4030f20
niksa <cvorovicniksa@gmail.com>
2025-06-24 00:52:39 +0200

    Dodati algoritmi za selekciju. Sastavljen genetski algoritam

--- main.py
+++ main.py
@@ -2,19 +2,15 @@ from ga import *
 from graphics import *
 
 FILE_NAME = "./input/in1.txt"
-stock_width = 0
-stock_height = 0
-pieces = []
-piece_counts = {}
-
 def input_data():
     '''
     inputs test case from given file path FILE_NAME.
     :return:
     '''
-    global pieces, piece_counts, stock_width, stock_height
     with open(FILE_NAME, 'r') as f:
         stock_height, stock_width = map(int, f.readline().split())
+        pieces = []
+        piece_counts = {}
 
         # Read the number of piece types
         q = int(f.readline())
@@ -25,12 +21,13 @@ def input_data():
             pieces.append(Piece(p_width, p_height, id))
             piece_counts[id] = p_count
             id+=1
+    return stock_height, stock_width, pieces, piece_counts
 
 
 def main():
     seed(54)
     print("Inputing data...")
-    input_data()
+    stock_height, stock_width, pieces, piece_counts = input_data()
 
     print("Generating population...")
     print("stock_width:", stock_width, " stock_height:", stock_height)
@@ -38,7 +35,8 @@ def main():
     population = create_init_population(pieces, piece_counts, stock_width, stock_height)
     print("Init population done!")
 
-    # ga(population)
+    for i in range(50):
+        ga(population, piece_counts)
 
 
     visualise(population[0], stock_width, stock_height)
@@ -64,19 +62,5 @@ d. Create New Generation: The collection of mutated offspring now becomes the ne
 
 e. Termination Check: Check if you have reached a termination condition (e.g., max number of generations, or fitness has stopped improving). If not, loop back to Step 2: Fitness Evaluation with your new population.
 '''
-
-# METHDOS
-
-# crossoveer (prima 2 chromosoma, vraca 2 chromosoma) - urađeno                                     NIKSA
-# check_chromosome (prima chromosome, provera uslove + skrati broj gena ako je moguce) - urađeno    NIKSA
-# plotovanje resenja - urađeno                                                                      NIKSA
-# fitness_function  MOZDA NAM NE TREBA                                                              NIKSA
-
-
-# mutacija (prima chromosomm, vraca mutirani chromosome)                 VEDRAN
-# input piecova + velicina table                               uradjeno  VEDRAN
-# create_init_population                                       uradjeno  VEDRAN
-# natural_selection (80-20, nesto, treba uzimati i malo losih hromozoma) VEDRAN
-
 if __name__ == "__main__":
     main()
